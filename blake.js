b61663511527cfbba625fc33178ba695860158f3
FabBautis <FabBaut@gmail.com>
2022-02-21 21:16:24 -0700

    Wow I love web diesign
    Put Blake's image in index.html and created js folder

--- blake.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-window.addEventListener('mousemove', handleMouseMove);
-
-function handleMouseMove(event) {
-    var top = event.pageY - (cursor.clientHeight / 2);
-    var left = event.pageX - (cursor.clientWidth / 2);
-    cursor.style.top = top + 'px';
-    cursor.style.left = left + 'px';
-}
